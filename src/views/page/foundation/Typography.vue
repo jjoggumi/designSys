0f50e8d4cc94ca1afa504fbce8a9f45bffc8448f
김은영 <jjoggumi@gmail.com>
2024-07-26 09:03:46 +0900

    첫 커밋

---
+++ src/views/page/foundation/Typography.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="TypographySys">   
+    <article>    TypographySys
+    </article>
+  </main>
+</template>
+
+<script>
+export default {
+  name : 'FoundationTypography', 
+}
+</script>
